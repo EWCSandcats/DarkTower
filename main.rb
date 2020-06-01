b642502873f980cb61a5660330d0b196683507ff
Apple Jacker <ca+repl@eyedarts.net>
2020-06-01 20:40:30 +0000

    Add more rooms, change navigation system

--- main.rb
+++ main.rb
@@ -3,6 +3,7 @@
 # Then type `ruby main.rb` and press enter
 
 require 'yaml'
+require 'pry'
 
 module Utilities
   def clear_screen
@@ -23,13 +24,14 @@ module Utilities
 end
 
 class Room
+  include Utilities
+
   attr_reader :name, :description, :exits, :adjacent_rooms
 
-  def initialize(name, description, exits, adjacent_rooms)
+  def initialize(name, description, adjacent_rooms)
     @name = name
     @description = description
-    @exits = exits
-    @adjacent_rooms = adjacent_rooms
+    @adjacent_rooms = adjacent_rooms.split.map { |room| room.gsub('_', ' ')}
   end
 end
 
@@ -37,12 +39,7 @@ class Player
   include Utilities
 
   def initialize
-    @name = retrieve_name
     @score = 0
-  end
-
-  def to_s
-    name.capitalize
   end
 
   def increment_score
@@ -53,36 +50,20 @@ class Player
     @score = 0
   end
 
-  def retrieve_name
-    clear_screen
-    puts "What is your name?"
+  def retrieve_move(adjacent_rooms)
+    puts "\nWhere would you like to go?"
+    print ">> "
 
     loop do
       input = gets.chomp
-      return input if valid_name?(input)
-
-      puts "Sorry, your name must be 2-15 characters from a-z."
-    end
-  end
-
-  def valid_name?(input)
-    regex = /^[a-z]{2,15}$/i
-    input.match?(regex)
-  end
-
-  def retrieve_move(exits)
-    puts "\nWhat would you like to do?"
-
-    loop do
-      input = gets.chomp
-      return input if valid_move?(input, exits)
+      return input if valid_move?(input, adjacent_rooms)
 
       puts "Sorry, you cannot move in that direction."
     end
   end
 
-  def valid_move?(input, exits)
-    exits.include?(input.to_sym)
+  def valid_move?(input, adjacent_rooms)
+    adjacent_rooms.include?(input)
   end
 end
 
@@ -94,7 +75,7 @@ class MainGame
   def initialize
     @player = Player.new
     @rooms = load_rooms
-    @current_room = set_current_room("start")
+    @current_room = set_current_room("town_square")
 
     play
   end
@@ -103,26 +84,24 @@ class MainGame
     room_data = YAML.load_file('rooms.yml')
 
     room_data.map do |room|
-      room_exits = room.last["exits"].split.map(&:to_sym)
       Room.new(room.first,
                room.last["description"],
-               room_exits,
                room.last["adjacent_rooms"])
     end
   end
 
   def set_current_room(name)
-    @current_room = rooms.find { |room| room.name == name}
+    @current_room = rooms.find { |room| room.name == name.gsub(' ', '_') }
   end
 
   def play
     loop do
       clear_screen
-      puts "Room: #{current_room.name}\n\n"
+      puts "Room: #{current_room.name.split('_').map(&:capitalize).join(' ')}\n\n"
       puts current_room.description
-      puts "\nCurrent exits: #{joinor(current_room.exits)}"
-      direction = player.retrieve_move(current_room.exits)
-      set_current_room(current_room.adjacent_rooms[direction])
+      puts "\nAvailable actions: #{current_room.adjacent_rooms.join(' | ')}"
+      direction = player.retrieve_move(current_room.adjacent_rooms)
+      set_current_room(direction)
     end
   end
 end
